726e801c8d9b0aa5e691e0d4da079c8a63c72f91
V2Valdecantos <99716006+V2Valdecantos@users.noreply.github.com>
2024-09-26 22:34:06 +0800

    animation preview code

--- AppWindow.cpp
+++ AppWindow.cpp
@@ -1,4 +1,6 @@
 #include "AppWindow.h"
+#include <Windows.h>
+
 
 struct vec3
 {
@@ -8,8 +10,18 @@ struct vec3
 struct vertex
 {
 	vec3 position;
+	vec3 position1;
 	vec3 color;
+	vec3 color1;
 };
+
+
+__declspec(align(16))
+struct constant
+{
+	float m_angle;
+};
+
 
 AppWindow::AppWindow()
 {
@@ -31,23 +43,11 @@ void AppWindow::onCreate()
 
 	vertex list[] = 
 	{
-		//RAINBOW QUAD
-		{0.1f,0.1f,0.0f,   1,0,0}, // POS1
-		{0.1f,0.9f,0.0f,    0,1,0}, // POS2
-		{0.9f,0.1f,0.0f,   0,0,1},// POS2
-		{0.9f,0.9f,0.0f,    1,1,1},
-
-		//RAINBOW TRIANGLE
-		{-0.9f,0.1f,0.0f,   1,0,0},
-		{-0.5f,0.9f,0.0f,   0,1,0}, // POS1
-		{-0.1f,0.1f,0.0f,    0,0,1}, // POS2
-		
-
-		//GREEN QUAD
-		{-0.5f,-0.9f,0.0f,   0,0,0}, // POS1
-		{-0.5f,-0.1,0.0f,    1,1,0}, // POS2
-		{0.5f,-0.9f,0.0f,   0,0,1},// POS2
-		{0.5f,-0.1f,0.0f,    1,1,1},
+		//X - Y - Z
+		{-0.5f,-0.5f,0.0f,    -0.32f,-0.11f,0.0f,   0,0,0,  0,1,0 }, // POS1
+		{-0.5f,0.5f,0.0f,     -0.11f,0.78f,0.0f,    1,1,0,  0,1,1 }, // POS2
+		{ 0.5f,-0.5f,0.0f,     0.75f,-0.73f,0.0f,   0,0,1,  1,0,0 },// POS2
+		{ 0.5f,0.5f,0.0f,      0.88f,0.77f,0.0f,    1,1,1,  0,0,1 }
 	};
 
 	m_vb=GraphicsEngine::get()->createVertexBuffer();
@@ -62,15 +62,17 @@ void AppWindow::onCreate()
 
 	GraphicsEngine::get()->releaseCompiledShader();
 
-	//RAINBOW PS
+
 	GraphicsEngine::get()->compilePixelShader(L"PixelShader.hlsl", "psmain", &shader_byte_code, &size_shader);
 	m_ps = GraphicsEngine::get()->createPixelShader(shader_byte_code, size_shader);
 	GraphicsEngine::get()->releaseCompiledShader();
 
-	//GREEN
-	GraphicsEngine::get()->compilePixelShader(L"GreenPixelShader.hlsl", "psmain", &shader_byte_code, &size_shader);
-	g_ps = GraphicsEngine::get()->createPixelShader(shader_byte_code, size_shader);
-	GraphicsEngine::get()->releaseCompiledShader();
+	constant cc;
+	cc.m_angle = 0;
+
+	m_cb = GraphicsEngine::get()->createConstantBuffer();
+	m_cb->load(&cc, sizeof(constant));
+
 }
 
 void AppWindow::onUpdate()
@@ -82,18 +84,32 @@ void AppWindow::onUpdate()
 	//SET VIEWPORT OF RENDER TARGET IN WHICH WE HAVE TO DRAW
 	RECT rc = this->getClientWindowRect();
 	GraphicsEngine::get()->getImmediateDeviceContext()->setViewportSize(rc.right - rc.left, rc.bottom - rc.top);
+
+	unsigned long new_time = 0;
+	if (m_old_time)
+		new_time = ::GetTickCount() - m_old_time;
+	m_delta_time = new_time / 1000.0f;
+	m_old_time = ::GetTickCount();
+
+	m_angle += 1.57f*m_delta_time;
+	constant cc;
+	cc.m_angle = m_angle;
+
+	m_cb->update(GraphicsEngine::get()->getImmediateDeviceContext(), &cc);
+
+	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_vs, m_cb);
+	GraphicsEngine::get()->getImmediateDeviceContext()->setConstantBuffer(m_ps, m_cb);
+
 	//SET DEFAULT SHADER IN THE GRAPHICS PIPELINE TO BE ABLE TO DRAW
 	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexShader(m_vs);
 	GraphicsEngine::get()->getImmediateDeviceContext()->setPixelShader(m_ps);
+
+
+	//SET THE VERTICES OF THE TRIANGLE TO DRAW
 	GraphicsEngine::get()->getImmediateDeviceContext()->setVertexBuffer(m_vb);
 
-	// draw the rainbow quad and triangle
-	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(7, 0);
-
-	//switch shaders and draw the quad
-	GraphicsEngine::get()->getImmediateDeviceContext()->setPixelShader(g_ps);
-	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(4, 7);
-
+	// FINALLY DRAW THE TRIANGLE
+	GraphicsEngine::get()->getImmediateDeviceContext()->drawTriangleStrip(m_vb->getSizeVertexList(), 0);
 	m_swap_chain->present(true);
 }
 
